62c79d327ab354eeb013f8b422f3c5d1aff7fc77
vaibha987 <v2goyal95@gmail.com>
2024-07-27 13:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	let newRow=table.insertRow(0)
 	let cell1=newRow.insertCell(0)
 	let cell2=newRow.insertCell(1)
-	cell1.innerText="New cell1"
-	cell2.innerText="New cell2"
+	cell1.innerText="New Cell1"
+	cell2.innerText="New Cell2"
 	
   
   
