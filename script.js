50867f33b7af354209a7cab73e578ca6ee50fac3
vaibha987 <v2goyal95@gmail.com>
2024-08-25 18:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function insert_Row() {
     //Write your code here
-	let table=document.getElementById("sampleTable")
+	// Appraoch 1 (insert row and cell table begin)
+	let table=document.getElementById('sampleTable');
+	let newRow=table.insertRow(0)
+	let cell1=newRow.insertCell(0)
+	let cell2=newRow.insertCell(1)
+	cell1.innerText="New Cell1"
+	cell2.innerText="New Cell2" 
+
+	// Approach 2 (insert row and cell table end)
+	/*let table=document.getElementById("sampleTable")
 	let row=document.createElement('tr')
 	let cell1=document.createElement('td')
 	cell1.innerText="New Cell1"
@@ -9,5 +18,5 @@ function insert_Row() {
 	let rowCell=row.appendChild(cell1)
 	rowCell=row.appendChild(cell2)
 	table.appendChild(row)
-	
+	*/
 }
